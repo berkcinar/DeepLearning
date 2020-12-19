d227335a4ce9d1817daf7d323f4f2c63803d0cb1
berkcinar <44802680+berkcinar@users.noreply.github.com>
2020-12-20 02:32:51 +0300

    Create call_back_example.py

---
+++ call_back_example.py
@@ -0,0 +1,6 @@
+# Define a Callback class that stops training once accuracy reaches 97.0%
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.97):
+      print("\nReached 97.0% accuracy so cancelling training!")
+      self.model.stop_training = True

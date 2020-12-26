3beb492e01ded91f405bf579b5777f5e56085110
berkcinar <44802680+berkcinar@users.noreply.github.com>
2020-12-26 20:33:45 +0300

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -28,3 +28,18 @@ model = tf.keras.Sequential([
 ])
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.summary()
+
+
+#plot
+import matplotlib.pyplot as plt
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+    
+    
+plot_graphs(history, 'accuracy')

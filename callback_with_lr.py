cf06740663fed2409d0fe1cc90c9974adfb9306f
berkcinar <44802680+berkcinar@users.noreply.github.com>
2020-12-28 00:36:04 +0300

    Create callback_with_lr.py

---
+++ callback_with_lr.py
@@ -0,0 +1,14 @@
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(10, input_shape=[window_size], activation="relu"), 
+    tf.keras.layers.Dense(10, activation="relu"), 
+    tf.keras.layers.Dense(1)
+])
+
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(
+    lambda epoch: 1e-8 * 10**(epoch / 20))
+optimizer = tf.keras.optimizers.SGD(lr=1e-8, momentum=0.9)
+model.compile(loss="mse", optimizer=optimizer)
+history = model.fit(dataset, epochs=100, callbacks=[lr_schedule], verbose=0)

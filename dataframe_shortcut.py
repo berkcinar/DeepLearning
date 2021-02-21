0e2a5b01c62967ef2055f6370fd329a5e46ac8b3
berkcinar <44802680+berkcinar@users.noreply.github.com>
2021-02-21 16:06:54 +0300

    Update dataframe_shortcut.py

--- dataframe_shortcut.py
+++ dataframe_shortcut.py
@@ -5,5 +5,11 @@ np.random.seed(0)
 left_df = pd.DataFrame({'transaction_id': ['A', 'B', 'C', 'D'], 'user_id': ['Peter', 'John', 'John', 'Anna'],'value': np.random.randn(4),})
 # users
 right_df = pd.DataFrame({'user_id': ['Paul', 'Mary', 'John','Anna'],'favorite_color': ['blue', 'blue', 'red', np.NaN],})
+
+
 #left join example
 left_df.merge(right_df, on='user_id', how='left')
+
+
+#drop column
+df.drop(columns=['B', 'C'])
